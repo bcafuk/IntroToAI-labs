724fdeed23f628c56ed1e524c44a069d369c8866
Borna Cafuk <borna.cafuk@gmail.com>
2021-03-28 14:21:46 +0200

    Implement A*

--- lab1java/src/main/java/ui/Algorithms.java
+++ lab1java/src/main/java/ui/Algorithms.java
@@ -51,6 +51,60 @@ public final class Algorithms {
     });
 
     public static final Algorithm A_STAR_SEARCH = ((stateSpace, heuristic) -> {
+        Comparator<Node> nodeComparator = (node1, node2) -> {
+            double estCost1 = node1.accumulatedCost + heuristic.getEstimatedCost(node1.state);
+            double estCost2 = node2.accumulatedCost + heuristic.getEstimatedCost(node2.state);
+
+            if (estCost1 == estCost2)
+                return node1.state.compareTo(node2.state);
+
+            return Double.compare(estCost1, estCost2);
+        };
+
+        Map<String, Node> openNodes = new HashMap<>();
+        TreeMap<Node, Node> open = new TreeMap<>(nodeComparator);
+        Node initialNode = new Node(stateSpace.getInitialState());
+
+        open.put(initialNode, initialNode);
+        openNodes.put(initialNode.state, initialNode);
+
+        Map<String, Double> closed = new HashMap<>();
+
+        while (!open.isEmpty()) {
+            Node n = open.firstKey();
+            open.remove(n);
+            openNodes.remove(n.state);
+
+            closed.put(n.state, n.accumulatedCost);
+
+            if (stateSpace.isGoalState(n.state))
+                return new Algorithm.SearchResult(n, closed.size());
+
+            for (StateSpace.Successor s : stateSpace.getSuccessors(n.state)) {
+                Node m = n.constructChild(s);
+
+                if (openNodes.containsKey(m.state)) {
+                    Node mPrime = openNodes.get(m.state);
+                    if (mPrime.accumulatedCost < m.accumulatedCost) {
+                        continue;
+                    } else {
+                        open.remove(mPrime);
+                        openNodes.remove(mPrime.state);
+                    }
+                }
+
+                if (closed.containsKey(m.state)) {
+                    if (closed.get(m.state) < m.accumulatedCost)
+                        continue;
+                    else
+                        closed.remove(m.state);
+                }
+
+                open.put(m, m);
+                openNodes.put(m.state, m);
+            }
+        }
+
         return null;
     });
 }
