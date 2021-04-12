08cf3d91d020aa3dee419a0181dc872fda27b1c9
Borna Cafuk <borna.cafuk@gmail.com>
2021-04-12 19:56:11 +0200

    Add Clause class and implement parsing clauses

---
+++ lab2java/src/main/java/ui/Clause.java
@@ -0,0 +1,119 @@
+package ui;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Clause {
+    private final Map<String, Boolean> literals = new HashMap<>();
+
+    public final Clause parent1;
+    public final Clause parent2;
+
+    public Clause() {
+        this(null, null);
+    }
+
+    public Clause(Clause parent1, Clause parent2) {
+        this.parent1 = parent1;
+        this.parent2 = parent2;
+    }
+
+    public boolean isNil() {
+        return literals.isEmpty();
+    }
+
+    public boolean subsumes(Clause that) {
+        for (var entry : that.literals.entrySet())
+            if (this.literals.get(entry.getKey()) != entry.getValue())
+                return false;
+
+        return true;
+    }
+
+    public Set<Clause> inverse() {
+        Set<Clause> clauses = new HashSet<>();
+
+        for (var entry : literals.entrySet()) {
+            Clause clause = new Clause();
+            clause.literals.put(entry.getKey(), !entry.getValue());
+            clauses.add(clause);
+        }
+
+        return clauses;
+    }
+
+    public static Clause resolve(Clause a, Clause b) {
+        Clause result = new Clause(a, b);
+
+        result.literals.putAll(a.literals);
+
+        boolean resolved = false;
+        for (var bEntry : b.literals.entrySet()) {
+            Boolean aValue = result.literals.get(bEntry.getKey());
+
+            if (aValue == null) {
+                result.literals.put(bEntry.getKey(), bEntry.getValue());
+                continue;
+            }
+
+            if (aValue.equals(bEntry.getValue()))
+                continue;
+
+            if (resolved)
+                return null;
+
+            result.literals.remove(bEntry.getKey());
+            resolved = true;
+        }
+
+        return resolved ? result : null;
+    }
+
+    public static Clause parse(String string) {
+        Clause clause = new Clause();
+
+        String[] split = string.toLowerCase().split("\\s");
+        for (int i = 0; i < split.length; i++) {
+            if (i % 2 == 1) {
+                if (split[i].equals("v"))
+                    continue;
+                else
+                    throw new IllegalArgumentException("Expected v, got " + split[i]);
+            }
+
+            boolean value = !split[i].startsWith("~");
+            String atom = value ? split[i] : split[i].substring(1);
+
+            Boolean prevValue = clause.literals.put(atom, value);
+            if (prevValue != null && prevValue != value)
+                throw new IllegalArgumentException("Tautology for atom " + atom);
+        }
+
+
+        return clause;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Clause clause = (Clause) o;
+        return literals.equals(clause.literals);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(literals);
+    }
+
+    @Override
+    public String toString() {
+        if (isNil())
+            return "NIL";
+
+        return literals.entrySet()
+                       .stream()
+                       .map(entry -> (entry.getValue() ? "" : "~") + entry.getKey())
+                       .collect(Collectors.joining(" v "));
+    }
+}
--- lab2java/src/main/java/ui/Solution.java
+++ lab2java/src/main/java/ui/Solution.java
@@ -1,12 +1,47 @@
 package ui;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Solution {
+    public static void main(String... args) {
+        String mode = args[0];
 
-	public static void main(String ... args) {
-		System.out.println("Ovime kreće Vaš program.");
-		for(String arg : args) {
-			System.out.printf("Predan argument %s%n", arg);
-		}
-	}
+        switch (mode) {
+            case "resolution" -> {
+                String clauseFile = args[1];
 
+                List<Clause> clauses;
+
+                try {
+                    clauses = parseClauses(Files.lines(Path.of(clauseFile)));
+                } catch (IOException e) {
+                    System.err.println("Error reading clauses from path " + clauseFile);
+
+                    System.exit(1);
+                    return;
+                }
+
+                Clause goal = clauses.remove(clauses.size() - 1);
+                clauses.addAll(goal.inverse());
+            }
+            case "cookbook" -> {
+                String clauseFile = args[1];
+                String commandFile = args[2];
+
+                throw new RuntimeException("Not yet implemented");
+            }
+            default -> throw new IllegalArgumentException("Unknown mode " + mode);
+        }
+    }
+
+    private static List<Clause> parseClauses(Stream<String> lines) {
+        return lines.filter(line -> !line.startsWith("#"))
+                    .map(Clause::parse)
+                    .collect(Collectors.toList());
+    }
 }
