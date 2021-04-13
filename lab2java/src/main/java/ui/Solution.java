8d9b143c284c9e4b7e7a46d776c22a4a4c1e161f
Borna Cafuk <borna.cafuk@gmail.com>
2021-04-13 13:19:47 +0200

    Extract modes into separate methods

--- lab2java/src/main/java/ui/Solution.java
+++ lab2java/src/main/java/ui/Solution.java
@@ -10,108 +10,92 @@ public class Solution {
     public static void main(String... args) {
         String mode = args[0];
 
-        switch (mode) {
-            case "resolution" -> {
-                String clauseFile = args[1];
-
-                List<String> lines;
-
-                try {
-                    lines = Files.lines(Path.of(clauseFile))
-                                 .filter(line -> !line.startsWith("#"))
-                                 .collect(Collectors.toList());
-                } catch (IOException e) {
-                    System.err.println("Error reading clauses from path " + clauseFile);
-
-                    System.exit(1);
-                    return;
-                }
-
-                String goalText = lines.remove(lines.size() - 1);
-
-                List<Clause> clauses = lines.stream()
-                                            .map(Clause::parse)
-                                            .filter(Objects::nonNull)
-                                            .collect(Collectors.toList());
-                Clause goal = Clause.parse(goalText);
-
-                Clause result;
-                if (goal == null)
-                    result = new Clause();
-                else
-                    result = RefutationResolver.resolution(clauses, goal);
-
-                System.out.println(clauseTrace(result));
-
-                System.out.print("[CONCLUSION]: " + goalText.toLowerCase() + " is ");
-                if (result == null)
-                    System.out.println("unknown");
-                else
-                    System.out.println("true");
+        try {
+            switch (mode) {
+                case "resolution" -> resolution(args[1]);
+                case "cooking" -> cooking(args[1], args[2]);
+                default -> throw new IllegalArgumentException("Unknown mode " + mode);
             }
-            case "cooking" -> {
-                String clauseFile = args[1];
-                String commandFile = args[2];
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 
-                Set<Clause> clauses;
+    private static void resolution(String clauseFile) throws IOException {
+        List<String> lines;
 
-                try {
-                    clauses = Files.lines(Path.of(clauseFile))
+        lines = Files.lines(Path.of(clauseFile))
+                     .filter(line -> !line.startsWith("#"))
+                     .collect(Collectors.toList());
+
+        String goalText = lines.remove(lines.size() - 1);
+
+        List<Clause> clauses = lines.stream()
+                                    .map(Clause::parse)
+                                    .filter(Objects::nonNull)
+                                    .collect(Collectors.toList());
+        Clause goal = Clause.parse(goalText);
+
+        Clause result;
+        if (goal == null)
+            result = new Clause();
+        else
+            result = RefutationResolver.resolution(clauses, goal);
+
+        if (result != null)
+            System.out.println(clauseTrace(result));
+
+        System.out.print("[CONCLUSION]: " + goalText.toLowerCase() + " is ");
+        if (result == null)
+            System.out.println("unknown");
+        else
+            System.out.println("true");
+    }
+
+    private static void cooking(String clauseFile, String commandFile) throws IOException {
+        Set<Clause> clauses = Files.lines(Path.of(clauseFile))
                                    .filter(line -> !line.startsWith("#"))
                                    .map(Clause::parse)
                                    .filter(Objects::nonNull)
                                    .collect(Collectors.toCollection(LinkedHashSet::new));
-                } catch (IOException e) {
-                    System.err.println("Error reading clauses from path " + clauseFile);
 
-                    System.exit(1);
-                    return;
-                }
+        Files.lines(Path.of(commandFile))
+             .forEachOrdered(line -> {
+                 char command = line.charAt(line.length() - 1);
+                 String clauseText = line.substring(0, line.length() - 2);
+                 Clause clause = Clause.parse(clauseText);
 
-                try {
-                    Files.lines(Path.of(commandFile))
-                         .forEachOrdered(line -> {
-                             char command = line.charAt(line.length() - 1);
-                             String clauseText = line.substring(0, line.length() - 2);
-                             Clause clause = Clause.parse(clauseText);
+                 switch (command) {
+                     case '+' -> {
+                         if (clause != null)
+                             clauses.add(clause);
+                     }
+                     case '-' -> clauses.remove(clause);
+                     case '?' -> {
+                         Clause result;
 
-                             switch (command) {
-                                 case '+' -> {
-                                     if (clause != null) clauses.add(clause);
-                                 }
-                                 case '-' -> clauses.remove(clause);
-                                 case '?' -> {
-                                     Clause result;
+                         if (clause == null)
+                             result = null;
+                         else
+                             result = RefutationResolver.resolution(new LinkedList<>(clauses), clause);
 
-                                     if (clause == null)
-                                         result = null;
-                                     else
-                                         result = RefutationResolver.resolution(new LinkedList<>(clauses), clause);
+                         System.out.println();
+                         if (result != null)
+                             System.out.println(clauseTrace(result));
 
-                                     System.out.print("[CONCLUSION]: " + clauseText.toLowerCase() + " is ");
-                                     if (result == null)
-                                         System.out.println("unknown");
-                                     else
-                                         System.out.println("true");
-                                 }
-                                 default -> throw new RuntimeException("Illegal command " + command);
-                             }
-                         });
-                } catch (IOException e) {
-                    System.err.println("Error reading commands from path " + commandFile);
-
-                    System.exit(1);
-                    return;
-                }
-            }
-            default -> throw new IllegalArgumentException("Unknown mode " + mode);
-        }
+                         System.out.print("[CONCLUSION]: " + clauseText.toLowerCase() + " is ");
+                         if (result == null)
+                             System.out.println("unknown");
+                         else
+                             System.out.println("true");
+                     }
+                     default -> throw new RuntimeException("Illegal command " + command);
+                 }
+             });
     }
 
     public static String clauseTrace(Clause clause) {
-        if (clause == null)
-            return "";
-
         LinkedHashMap<Clause, Integer> indices = new LinkedHashMap<>();
         traverseIndices(clause, indices);
 
