ca3c09b8abbbf7fa34059d6e76f5c5df29f0124b
Borna Cafuk <borna.cafuk@gmail.com>
2021-05-18 19:14:01 +0200

    Begin implementing Dataset class

---
+++ lab3java/src/main/java/ui/Dataset.java
@@ -0,0 +1,98 @@
+package ui;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Dataset {
+    private final String[] labels;
+    private final List<String[]> data;
+
+    private Dataset(String[] labels) {
+        this(labels, new ArrayList<>());
+    }
+
+    private Dataset(String[] labels, List<String[]> data) {
+        this.labels = Objects.requireNonNull(labels);
+        this.data = Objects.requireNonNull(data);
+
+        for (String[] datum : data)
+            if (datum.length != labels.length)
+                throw new IllegalArgumentException("Length mismatch: a row contains " + datum.length + " values, but " + labels.length + " were expected.");
+    }
+
+    private void addDatum(String[] datum) {
+        Objects.requireNonNull(datum);
+        if (datum.length != labels.length)
+            throw new IllegalArgumentException("Length mismatch: the datum contains " + datum.length + " values, but " + labels.length + " were expected.");
+
+        data.add(datum);
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    public int numData() {
+        return data.size();
+    }
+
+    public int getFeatureCount() {
+        return labels.length;
+    }
+
+    public String getLabelForIndex(int index) {
+        return labels[index];
+    }
+
+    public int getIndexForLabel(String label) {
+        Objects.requireNonNull(label);
+
+        for (int i = 0; i < labels.length; i++)
+            if (labels[i].equals(label))
+                return i;
+
+        throw new NoSuchElementException("The dataset does not contain the label " + label);
+    }
+
+    public String mostFrequentFor(int featureIndex) {
+        Map<String, Integer> frequencies = new HashMap<>();
+
+        for (String[] datum : data)
+            frequencies.merge(datum[featureIndex], 1, (f, df) -> f + 1);
+
+        return frequencies.entrySet()
+                          .stream()
+                          .max(Map.Entry.comparingByValue())
+                          .orElseThrow()
+                          .getKey();
+    }
+
+    public Map<String, Dataset> splitBy(int featureIndex) {
+        Map<String, Dataset> subsets = new TreeMap<>();
+
+        for (String[] datum : data)
+            subsets.computeIfAbsent(datum[featureIndex], v -> new Dataset(labels))
+                   .addDatum(datum);
+
+        return subsets;
+    }
+
+    // TODO: Implement information gain
+
+    public static Dataset parse(Stream<String> lines) {
+        Objects.requireNonNull(lines);
+
+        LinkedList<String[]> data = lines.map(line -> line.split(","))
+                                         .collect(Collectors.toCollection(LinkedList::new));
+
+        String[] labels = data.remove(0);
+
+
+        return new Dataset(labels, data);
+    }
+
+    public static boolean labelsMatch(Dataset d1, Dataset d2) {
+        return Arrays.equals(d1.labels, d2.labels);
+    }
+}
--- lab3java/src/main/java/ui/Solution.java
+++ lab3java/src/main/java/ui/Solution.java
@@ -1,11 +1,30 @@
 package ui;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Solution {
 
 	public static void main(String ... args) {
-		System.out.println("Ovime kreće Vaš program.");
-		for(String arg : args) {
-			System.out.printf("Predan argument %s%n", arg);
+		String trainingFilename = args[0];
+		String testingFilename = args[1];
+
+		Dataset trainingData;
+		Dataset testingData;
+		try {
+			trainingData = Dataset.parse(Files.lines(Path.of(trainingFilename)));
+			testingData = Dataset.parse(Files.lines(Path.of(testingFilename)));
+		} catch (IOException e) {
+			System.err.println("Couldn't open file " + e.getMessage());
+			System.exit(1);
+			return;
+		}
+
+		if (!Dataset.labelsMatch(trainingData, testingData)) {
+			System.err.println("The training and test sets' labels don't match");
+			System.exit(1);
+			return;
 		}
 	}
 
