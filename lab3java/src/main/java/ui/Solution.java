77447382dfe3cab83643f1870d2706d7aa0c74e6
Borna Cafuk <borna.cafuk@gmail.com>
2021-05-19 17:58:59 +0200

    Reindent with spaces

--- lab3java/src/main/java/ui/Solution.java
+++ lab3java/src/main/java/ui/Solution.java
@@ -6,42 +6,42 @@ import java.nio.file.Path;
 
 public class Solution {
 
-	public static void main(String ... args) {
-		String trainingFilename = args[0];
-		String testingFilename = args[1];
+    public static void main(String... args) {
+        String trainingFilename = args[0];
+        String testingFilename = args[1];
 
-		Dataset trainingData;
-		Dataset testingData;
-		try {
-			trainingData = Dataset.parse(Files.lines(Path.of(trainingFilename)));
-			testingData = Dataset.parse(Files.lines(Path.of(testingFilename)));
-		} catch (IOException e) {
-			System.err.println("Couldn't open file " + e.getMessage());
-			System.exit(1);
-			return;
-		}
+        Dataset trainingData;
+        Dataset testingData;
+        try {
+            trainingData = Dataset.parse(Files.lines(Path.of(trainingFilename)));
+            testingData = Dataset.parse(Files.lines(Path.of(testingFilename)));
+        } catch (IOException e) {
+            System.err.println("Couldn't open file " + e.getMessage());
+            System.exit(1);
+            return;
+        }
 
-		if (!Dataset.labelsMatch(trainingData, testingData)) {
-			System.err.println("The training and test sets' labels don't match");
-			System.exit(1);
-			return;
-		}
+        if (!Dataset.labelsMatch(trainingData, testingData)) {
+            System.err.println("The training and test sets' labels don't match");
+            System.exit(1);
+            return;
+        }
 
-		int classIndex = trainingData.getFeatureCount() - 1;
+        int classIndex = trainingData.getFeatureCount() - 1;
 
-		ID3 id3 = new ID3();
+        ID3 id3 = new ID3();
 
-		id3.fit(trainingData, classIndex);
+        id3.fit(trainingData, classIndex);
 
-		System.out.println("[BRANCHES]:");
-		System.out.print(id3.toString());
+        System.out.println("[BRANCHES]:");
+        System.out.print(id3.toString());
 
-		System.out.print("[PREDICTIONS]:");
-		for (String[] datum : testingData) {
-			System.out.print(' ');
-			System.out.print(id3.predict(datum));
-		}
-		System.out.println();
-	}
+        System.out.print("[PREDICTIONS]:");
+        for (String[] datum : testingData) {
+            System.out.print(' ');
+            System.out.print(id3.predict(datum));
+        }
+        System.out.println();
+    }
 
 }
