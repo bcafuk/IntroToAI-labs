5c884868a25b8586fb4b8c7c5e9c1f2bcf791ca2
Borna Cafuk <borna.cafuk@gmail.com>
2021-05-19 18:13:43 +0200

    Print confusion matrix

--- lab3java/src/main/java/ui/Solution.java
+++ lab3java/src/main/java/ui/Solution.java
@@ -3,6 +3,7 @@ package ui;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
 
 public class Solution {
 
@@ -42,20 +43,58 @@ public class Solution {
         System.out.print(id3.toString());
 
         int correctPredictions = 0;
+        Set<String> classLabels = new TreeSet<>();
+        Map<ClassPair, Integer> confusion = new HashMap<>();
 
         System.out.print("[PREDICTIONS]:");
         for (String[] datum : testingData) {
             String prediction = id3.predict(datum);
+            String expected = datum[classIndex];
 
             System.out.print(' ');
             System.out.print(prediction);
 
-            if (prediction.equals(datum[classIndex]))
+            if (prediction.equals(expected))
                 correctPredictions++;
+
+            classLabels.add(prediction);
+            classLabels.add(expected);
+            confusion.merge(new ClassPair(prediction, expected), 1, (f, dv) -> f + 1);
         }
         System.out.println();
 
         System.out.printf("[ACCURACY]: %.5f\n", (double)correctPredictions / testingData.numData());
+
+        System.out.println("[CONFUSION_MATRIX]:");
+        for (String expectedClass : classLabels) {
+            for (String predictedClass : classLabels) {
+                System.out.print(confusion.getOrDefault(new ClassPair(predictedClass, expectedClass), 0));
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
     }
 
+    private static class ClassPair {
+        public String prediction;
+        public String expected;
+
+        public ClassPair(String prediction, String expected) {
+            this.prediction = Objects.requireNonNull(prediction);
+            this.expected = Objects.requireNonNull(expected);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ClassPair classPair = (ClassPair) o;
+            return prediction.equals(classPair.prediction) && expected.equals(classPair.expected);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(prediction, expected);
+        }
+    }
 }
